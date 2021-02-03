4c715dd903cc3c66d6ee7cc4cfa58f923bdc53e9
macenuw <gnykabiny54@gmail.com>
2021-02-03 18:27:14 +0200

    first project JS

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
-let number  = 5;
-const leftBorderWidth = 1;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', '');
+
+const personalMovieDB = {
+count: numberOfFilms,
+movies: {},
+actors: {},
+genres: [],
+privat: false
+};
+
+const a = prompt('Один из последних просмотреных фильмов ?','');
+const b = +prompt('На сколько вы оцените его?','');
+const c = prompt('Один из последних просмотреных фильмов ?','');
+const d = +prompt('На сколько вы оцените его?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB)
